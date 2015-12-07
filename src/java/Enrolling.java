2501395388ed59e9a96f9e38eb8190306a2e9569
daiker0330 <daiker0330@gmail.com>
2015-12-06 21:24:37 -0800

    create

---
+++ src/java/Enrolling.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Lab6;
+
+/**
+ *
+ * @author yunfan
+ */
+public class Enrolling extends AnyImpact {
+
+    public void addCourse(Course c){
+        this.getArr().add(c);
+    }
+}
---
+++ src/java/searchHistory.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public class searchHistory extends AnyLog{
+
+	public void addHistory(String s){
+		super.addElementLog(s);
+	}
+	public String printHistory(){
+		return super.printLog();
+	}
+}
